82050629b4c7362ce9dfb93dffd70098c6fbef85
FrankieRM <frodriguezm17@gmail.com>
2018-06-14 15:45:00 -0500

    implementando repository

--- src/main/java/com/osp/demo/movietheater/MovieTheaterApplication.java
+++ src/main/java/com/osp/demo/movietheater/MovieTheaterApplication.java
@@ -1,12 +1,34 @@
 package com.osp.demo.movietheater;
 
+import com.osp.demo.movietheater.builder.MovieBuilder;
+import com.osp.demo.movietheater.domain.MovieStatus;
+import com.osp.demo.movietheater.repository.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovieTheaterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieTheaterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTheaterApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(MovieRepository movieRepository) {
+        return args -> Arrays.asList("Frank Rodríguez", "Hugo Gonzáles", "Jhon Díaz")
+                .forEach(name -> {
+                            movieRepository.save(new MovieBuilder()
+                                    .fullName(name)
+                                    .publishingDate(LocalDateTime.now().toString())
+                                    .status(MovieStatus.ACTIVE.toString())
+                                    .build());
+                            movieRepository.findAll();
+                        }
+                );
+    }
 }
---
+++ src/main/java/com/osp/demo/movietheater/domain/MovieStatus.java
@@ -0,0 +1,11 @@
+package com.osp.demo.movietheater.domain;
+
+/**
+ * Created on 14/06/2018.
+ *
+ * @author Entelgy
+ */
+public enum MovieStatus {
+    ACTIVE,
+    INACTIVE
+}
